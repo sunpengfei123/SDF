0ec4bbb343c5148234ddc9416c5087c270b7610c
sunpengfei123 <18811500659@139.com>
2020-07-20 15:46:58 +0800

    huawei

--- optimizationtest/Retime_HSDFTest.py
+++ optimizationtest/Retime_HSDFTest.py
@@ -12,13 +12,13 @@ a = DV.Vertex('a', 1)
 b = DV.Vertex('b', 2)
 c = DV.Vertex('c', 3)
 
-e1 = DE.SDFedge('e1', 1, 1, 1)
-e2 = DE.SDFedge('e2', 0, 1, 1)
-e3 = DE.SDFedge('e3', 1, 1, 1)
-
-# e1 = DE.SDFedge('e1', 0, 1, 1)
+# e1 = DE.SDFedge('e1', 1, 1, 1)
 # e2 = DE.SDFedge('e2', 0, 1, 1)
 # e3 = DE.SDFedge('e3', 1, 1, 1)
+
+e1 = DE.SDFedge('e1', 1, 1, 1)
+e2 = DE.SDFedge('e2', 1, 1, 1)
+e3 = DE.SDFedge('e3', 0, 1, 1)
 
 g = SDFG.SDFgraph('Retime_Hsdf_Test')
 g.addVertex(a)
